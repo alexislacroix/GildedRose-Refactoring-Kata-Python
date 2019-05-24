d538dfa71f9c055dbe040f359f83567acda94aa1
Wernard Berber <alexis.lacroix80@gmail.com>
2019-05-24 22:52:13 +0200

    'gilded_rose.by' refactoring by adding new inheriting Item classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,34 +6,45 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+
+            # ###################
+            # set class item
+            # ###################
+
+            # tree of class
+            # Item < Simple_Item < Perishable_Item < VerryPerishable_Item
+            #                    < TakeValue_Item  < Event_Item
+            #                    < Legendary_Item
+
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                item.__class__ = Legendary_Item
+
+            elif item.name == "Aged Brie":
+                item.__class__ = TakeValue_Item
+
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.__class__ = Event_Item
+
+            elif item.name == "Conjured Mana Cake":
+                item.__class__ = VerryPerishable_Item
+
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                item.__class__ = Perishable_Item
+
+            # ###################
+            # update item
+            # ###################
+
+            # update quality
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                item.update_quality()
+            else:
+                item.update_quality([-1,10,5])
+
+            # update sell_in
+            item.update_sell_in()
 
 
 class Item:
@@ -44,3 +55,71 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class Simple_Item(Item):
+
+    def update_quality(self):
+        pass
+
+    def update_sell_in(self):
+
+        # sell_in -1
+        self.sell_in -= 1
+
+
+class Perishable_Item(Simple_Item):
+
+    def update_quality(self):
+
+        # quality -1 if sell_in >= 0
+        if self.sell_in >= 0 :
+            self.quality -= 1
+
+        # quality -2 if sell_in < 0
+        else:
+            self.quality -=2
+
+        # quality always > 0
+        if self.quality < 0:
+            self.quality = 0
+
+class VerryPerishable_Item(Perishable_Item):
+
+    def update_quality(self):
+        Perishable_Item.update_quality(self)
+        Perishable_Item.update_quality(self)
+
+class TakeValue_Item(Simple_Item):
+
+    def update_quality(self, steps = [-1]):
+
+        for step in steps:
+
+            # normal case (unknown step)
+            # -1 <=> unknown step
+            if step == -1 :
+                self.quality += 1
+
+            # special case
+            else:
+                if self.sell_in <= step :
+                    self.quality += 1
+
+        # quality always < 50
+        if self.quality > 50:
+            self.quality = 50
+
+class Event_Item(TakeValue_Item):
+
+    def update_quality(self, steps = [-1]):
+
+        if self.sell_in > 0:
+            TakeValue_Item.update_quality(self, steps)
+        else:
+            self.quality = 0
+
+class Legendary_Item(Simple_Item):
+
+    def update_sell_in(self):
+        pass
